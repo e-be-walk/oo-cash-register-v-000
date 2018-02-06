03e631296b24f7ed8081efa214f269fdfa2d1684
Erin Walker <erinb0317@hotmail.com>
2018-02-06 15:16:19 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,10 +12,20 @@ class CashRegister
     @total
   end
 
-  def add_item()
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity 
+    @last_amount = price
+    quantity.times do 
+      @items << title
   end
 
-  def apply_discount()
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    else
+      @total = @total * (100-@discount)/100
+      "After the discount, the total comes to $#{@total}."
+    end
   end
 
 end
