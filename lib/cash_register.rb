99b9c7f7a74e4b09dc983fad2d524b2c1ad600cc
Erin Walker <erinb0317@hotmail.com>
2018-02-06 15:12:24 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,17 @@
 class CashRegister
   attr_accessor :total, :discount, :items, :last_amount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @last_amount = 0
+  end
+
+  def total
+    @total
+  end
+
   def add_item()
   end
 
